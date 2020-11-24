672a0a7468a64e1761179c48e6cf016f648ab446
fc277073030 <fc277073030@163.com>
2020-11-24 17:30:13 +0800

    init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='fanchao',
+      version='0.1',
+      description='fanchao test',
+      url='http://github.com/tongling/clinicaltrial',
+      author='fanchao',
+      author_email='fc277073030@163.com',
+      license='MIT',
+      packages=['fanchao', 'filter'],
+      zip_safe=False)
